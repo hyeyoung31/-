279c7c8c2ccde2dfddc7b506a98d8e5e1cd5b3eb
hyeyoung31 <154103645+hyeyoung31@users.noreply.github.com>
2024-03-12 23:35:38 +0900

    first commit
    유선배웹디자인기능사 학습 내용

---
+++ 웹디자인기능사javascript/js/01_main.js
@@ -0,0 +1,9 @@
+// document.addEventListener('DOMContentLoaded',()=>{
+
+// })
+
+
+let button =document.querySelector('button');
+button.addEventListener("click",function(){
+    alert('heoo world');
+})
