5b746d80aefdac745ffa066fd08d6058a1310a84
hyeyoung31 <154103645+hyeyoung31@users.noreply.github.com>
2024-03-16 13:00:28 +0900

    Update main.js
    [기본 슬라이드 구조]
    
    1. container 이동 시, margin 으로 이동. jQuery Animate 함수는 기본적으로 Transform이 작동하지 않음. Transform 속성 jquery animate 함수로 작동 시키기 위해 별도의 라이브러리가 필요.
    
    2.실무에서 슬라이드 개수가 변경되어도 구조가 틀어지지 않도록, container 너비를 스크립트를 통해 슬라이드 개수를 구해 너비를 늘리는 방식으로 구현할 것!

--- 03_슬라이드/01_좌우슬라이드 1형/BASE/script/main.js
+++ 03_슬라이드/01_좌우슬라이드 1형/BASE/script/main.js
@@ -27,3 +27,6 @@ function autoSlide(){
 },3000);   
 }
 autoSlide();
+
+// container 이동시, margin으로 이동. jQuery Animate 함수는 기본적으로 Transform이 작동하지 않음. Transform 속성 jquery animate 함수로 작동시키기 위해 별도의 라이브러리가 필요.
+// 실무에서 슬라이드개수가 변경되어도 구조가 틀어지지 않도록, container 너비를 스크립트를 통해 슬라이드 개수를 구해 너비를 늘리는 방식으로 구현할 것!
